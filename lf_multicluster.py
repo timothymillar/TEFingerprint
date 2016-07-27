3045aeaca0ef7380fce717be3f2eecb6f4ed45ec
Tim Millar <tim.millar@plantandfood.co.nz>
2016-07-27 13:51:52 +1200

    multicluster now calls splitcluster and has initial support for all argument flags

--- lf_multicluster.py
+++ lf_multicluster.py
@@ -1,32 +1,74 @@
-import os
+import sys
 import pysam
+import argparse
 from multiprocessing import Pool
-from subprocess import Popen, PIPE, STDOUT
-
-os.chdir('/Users/cfltxm/Repo/leapfrog')
-INFILE = "quick_test/data_full/testDanglersMappedSorted.bam"
+from lf_splitcluster import split_cluster
+from itertools import product
 
 
-s = pysam.view("-b", "-f", "16", INFILE, "chr11")
-p = Popen(['python', 'lf_cluster.py', '--reference', 'chr11'],
-          stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-result = p.communicate(input=s)[0]
-print(result.decode())
+def parse_args(args):
+    parser = argparse.ArgumentParser('Identify transposon flanking regions')
+    parser.add_argument('input_bam')
+    parser.add_argument('--references', nargs='?', default=False)
+    parser.add_argument('--read_groups', nargs='?', default=False)
+    parser.add_argument('--strands', nargs='?', default=True)
+    parser.add_argument('--eps', type=int, default=100)
+    parser.add_argument('--min_tips', type=int, default=5)
+    parser.add_argument('--cores', type=int, default=1)
+    return parser.parse_args(args)
 
 
-def cluster_job(bamfile, reference, family, reverse=False):
-    bam = pysam.view("-b", "-f", "16", bamfile, reference)
-    proc = Popen(['python', 'lf_cluster.py',  '--reference', reference],
-                 stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-    result = proc.communicate(input=bam)[0]
-    print(result.decode())
+def sam_references(input_bam):
+    sam = pysam.AlignmentFile(input_bam, 'rb')
+    references = sam.references
+    sam.close()
+    return references
 
 
-cluster_job(INFILE, "chr11", "family", False)
+def build_jobs(input_bam, references, read_groups, strands, eps, min_tips):
+    if references:
+        if isinstance(references, str):
+            references = [references]
+        else:
+            pass
+    else:
+        references = sam_references(input_bam)
+    if read_groups:
+        if isinstance(read_groups, str):
+            read_groups = [read_groups]
+        else:
+            pass
+    else:
+        read_groups = [False]
+    if strands is True:
+        strands = ['+', '-']
+    elif strands:
+        if isinstance(strands, str):
+            strands = [strands]
+        else:
+            pass
+    else:
+        strands = [False]
+    return product([input_bam],
+                   references,
+                   read_groups,
+                   strands,
+                   [eps],
+                   [min_tips])
 
-with Pool(5) as p:
-    p.starmap(cluster_job, [(INFILE, "chr11", "family", False),
-                            (INFILE, "chr12", "family", False)])
 
+def main():
+    args = parse_args(sys.argv[1:])
+    jobs = build_jobs(args.input_bam,
+                      args.references,
+                      args.read_groups,
+                      args.strands,
+                      args.eps,
+                      args.min_tips)
+    with Pool(args.cores) as p:
+        results = p.starmap(split_cluster, jobs)
+    for result in results:
+        print(result)
 
-
+if __name__ == '__main__':
+    main()
