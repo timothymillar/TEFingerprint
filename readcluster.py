753bf0a86813c4ce0d99332077154198d0edc9fd
Tim Millar <tim.millar@plantandfood.co.nz>
2016-07-29 13:49:59 +1200

    Convert numerics to string for string concat

--- readcluster.py
+++ readcluster.py
@@ -11,32 +11,32 @@ class ReadCluster(object):
         self.end = np.max(tips)
         self.count = len(tips)
         self.mean = np.mean(tips)
-        self.median = np.meadian(tips)
+        self.median = np.median(tips)
         self.mode = np.bincount(tips).argmax()
 
     def __id(self):
         return '_'.join((self.reference,
                          self.name,
                          self.strand,
-                         self.start))
+                         str(self.start)))
 
     def __attributes(self):
         template = 'ID={0};Name={1};count={2};mean={3};median={4};mode={5}'
         return template.format(self.__id(),
                                self.name,
-                               self.count,
-                               self.mean,
-                               self.median,
-                               self.mode)
+                               str(self.count),
+                               str(self.mean),
+                               str(self.median),
+                               str(self.mode))
 
     def __str__(self):
         template = "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}"
         return template.format(self.reference,
                                '.',
                                '.',
-                               self.start,
-                               self.end,
+                               str(self.start),
+                               str(self.end),
                                '.',
                                self.strand,
                                '.',
-                               self.attributes)
+                               self.__attributes())
