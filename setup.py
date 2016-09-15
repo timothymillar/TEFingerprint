07f28c79d9137ce9d3fa50a8b4f2b8a01e9c0871
Tim Millar <tim.millar@plantandfood.co.nz>
2016-09-15 15:08:48 +1200

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #! /usr/bin/env python
 
+import os
 from setuptools import setup
+
+
+def read_file(file_name):
+    return os.path.join(os.path.dirname(__file__), file_name)
+
 
 setup(name='tectoolkit',
       version='0.01',
-      url='https://github.com/PlantandFoodResearch/TECtoolkit',
       author='Tim Millar',
-      tests_require=['pytest'],
-      install_requires=[],
       author_email='tim.millar@plantandfood.co.nz',
+      url='https://github.com/PlantandFoodResearch/TECtoolkit',
+      description='Toolkit for identifying transposable element movement in regenerant clones',
+      long_description=read_file('README.MD'),
+      scripts=['applications'],
+      packages=['tectoolkit'],
+      classifiers=['Development Status :: 2 - Pre-Alpha']
       )
