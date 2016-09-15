75306d8e59724bb4a99ed10c06d737f4679d5064
Tim Millar <tim.millar@plantandfood.co.nz>
2016-09-15 13:58:47 +1200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tectoolkit',
+      version='0.01',
+      url='https://github.com/PlantandFoodResearch/TECtoolkit',
+      author='Tim Millar',
+      tests_require=['pytest'],
+      install_requires=[],
+      author_email='tim.millar@plantandfood.co.nz',
+      )
