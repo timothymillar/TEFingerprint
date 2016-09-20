9c88600f0806484aea73c2cd7b3d7c26e1e32beb
Tim Millar <tim.millar@plantandfood.co.nz>
2016-09-21 11:06:29 +1200

    Added GFFFeature and SAM_FLAG to classes.py

--- tectoolkit/classes.py
+++ tectoolkit/classes.py
@@ -3,6 +3,11 @@
 import numpy as np
 
 
+SAM_FLAGS = {'+': ('-F', '20'),
+             '-': ('-f', '16'),
+             '.': ('-F', '4')}
+
+
 class ReadGroup(object):
     """"""
     read = np.dtype([('tip', np.int64),
@@ -224,3 +229,47 @@ class ReferenceLoci(object):
         reference_loci = cls.from_simple_subcluster(points, minpts, eps)
         reference_loci.merge_overlapping()
         return reference_loci
+
+
+class GffFeature(object):
+    """"""
+    def __init__(self,
+                 seqid,
+                 source='.',
+                 ftype='.',
+                 start='.',
+                 end='.',
+                 score='.',
+                 strand='.',
+                 phase='.',
+                 **kwargs):
+        self.seqid = seqid
+        self.source = source
+        self.type = ftype
+        self.start = start
+        self.end = end
+        self.score = score
+        self.strand = strand
+        self.phase = phase
+        self.attributes = kwargs
+
+    def attribute_names(self):
+        return set(self.attributes.keys())
+
+    def __parse_attributes(self, attributes):
+        if attributes is not None:
+            return ';'.join(tuple('{0}={1}'.format(key, value) for key, value in attributes.items()))
+        else:
+            return'.'
+
+    def __str__(self):
+        template = "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}"
+        return template.format(self.seqid,
+                               self.source,
+                               self.type,
+                               self.start,
+                               self.end,
+                               self.score,
+                               self.strand,
+                               self.phase,
+                               self.__parse_attributes(self.attributes))
