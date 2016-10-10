f33f0755d017f6c8edc69e887ee770c7099cfb53
Tim Millar <tim.millar@plantandfood.co.nz>
2016-10-10 13:59:48 +1300

    Moved GffFeateure to gff.py

---
+++ tectoolkit/gff.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+
+class GffFeature(object):
+    """"""
+    def __init__(self,
+                 seqid,
+                 source='.',
+                 ftype='.',
+                 start='.',
+                 end='.',
+                 score='.',
+                 strand='.',
+                 phase='.',
+                 **kwargs):
+        self.seqid = seqid
+        self.source = source
+        self.type = ftype
+        self.start = start
+        self.end = end
+        self.score = score
+        self.strand = strand
+        self.phase = phase
+        self.attributes = kwargs
+
+    def attribute_names(self):
+        return set(self.attributes.keys())
+
+    def _parse_attributes(self, attributes):
+        if attributes is not None:
+            return ';'.join(tuple('{0}={1}'.format(key, value) for key, value in self.attributes.items()))
+        else:
+            return'.'
+
+    def __str__(self):
+        template = "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}"
+        return template.format(self.seqid,
+                               self.source,
+                               self.type,
+                               self.start,
+                               self.end,
+                               self.score,
+                               self.strand,
+                               self.phase,
+                               self._parse_attributes(self.attributes))
+
+if __name__ == '__main__':
+    pass
