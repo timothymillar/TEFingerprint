9711fb1ac8fae0eb58bcf42d00990fcc49f4ceb5
Tim Millar <tim.millar@plantandfood.co.nz>
2016-10-10 15:15:18 +1300

    Support for nested GFF features

--- tectoolkit/gff.py
+++ tectoolkit/gff.py
@@ -22,6 +22,7 @@ class GffFeature(object):
         self.strand = strand
         self.phase = phase
         self.attributes = kwargs
+        self.children = []
 
     def attribute_names(self):
         return set(self.attributes.keys())
@@ -31,6 +32,13 @@ class GffFeature(object):
             return ';'.join(tuple('{0}={1}'.format(key, value) for key, value in self.attributes.items()))
         else:
             return'.'
+
+    def add_children(self, *args):
+        for child in args:
+            assert isinstance(child, GffFeature)
+            assert "ID" in self.attributes
+            child.attributes["Parent"] = self.attributes["ID"]
+            self.children.append(child)
 
     def __str__(self):
         template = "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}"
@@ -43,6 +51,30 @@ class GffFeature(object):
                                self.strand,
                                self.phase,
                                self._parse_attributes(self.attributes))
+
+    def _str_nested(self):
+        return [self.__str__()] + [child._str_nested() for child in self.children]
+
+    def _flatten(self, item):
+        """
+
+        :param lst:
+        :return:
+        """
+        if isinstance(item, list):
+            for element in item:
+                for item in self._flatten(element):
+                    yield item
+        else:
+            yield item
+
+    def __format__(self, code):
+        assert code in {'nested', 'single'}
+        if code == "nested":
+            return '\n'.join(self._flatten(self._str_nested()))
+        else:
+            return self.__str__()
+
 
 if __name__ == '__main__':
     pass
