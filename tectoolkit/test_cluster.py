3546191c8dbf083630fd8a874877334be0eaeb56
Tim Millar <tim.millar@plantandfood.co.nz>
2016-10-25 16:00:49 +1300

    test for method _locus_points

--- tectoolkit/test_cluster.py
+++ tectoolkit/test_cluster.py
@@ -42,3 +42,11 @@ class TestUL:
                                 (4, 4)], dtype=_UnivariateLoci._ulocus)
         query._sort_uloci()
         npt.assert_array_equal(query.loci, answer.loci)
+
+    def test_locus_points(self):
+        """"""
+        query = np.array([5, 9, 4, 1, 6, 8, 6, 2], dtype=int)
+        locus = (5, 8)
+        answer = np.array([5, 6, 8, 6], dtype=int)
+        query = _UnivariateLoci._locus_points(locus, query)
+        npt.assert_array_equal(query, answer)
