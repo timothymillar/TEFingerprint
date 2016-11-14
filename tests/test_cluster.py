fd1112d73a2e3bce4d3a8c9081a6849970eacd0a
Tim Millar <tim.millar@plantandfood.co.nz>
2016-11-14 14:21:43 +1300

    Improved clarity of testsfor static methods in class HUDC

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -113,15 +113,14 @@ class TestHUDC:
         Produce a tree with one (root) node.
         Minimum eps of 3 means point 5 is still within eps of 3 and 7
         """
-        query = HUDC(3, 5, 3)  # Minimum eps of 3
-        query.points = np.array([1, 1, 2, 2, 2, 3, 3, 5, 7, 7, 8, 8, 9, 9, 9, 11])  # Point 5 within 3 of 3 and 7
+        query = np.array([1, 1, 2, 2, 2, 3, 3, 5, 7, 7, 8, 8, 9, 9, 9, 11])  # Point 5 within 3 of 3 and 7
         answer = {'area': 48,
                   'base_eps': 5,
                   'base_locus': (1, 11),
                   'child_area': 0,
                   'children': None,
                   'selected': False}
-        assert query._grow_tree(query.points, query.min_pts, query.max_eps, query.min_eps) == answer
+        assert HUDC._grow_tree(query, 3, 5, 3) == answer  # Minimum eps of 3
 
     def test_grow_tree_nested(self):
         """
@@ -129,8 +128,7 @@ class TestHUDC:
         Produce a tree with three nodes (root node and two child nodes).
         Minimum eps of 2 allows cluster to be split when points are not adjacent.
         """
-        query = HUDC(3, 5, 2)  # Minimum eps of 2
-        query.points = np.array([1, 1, 2, 2, 2, 3, 3, 5, 7, 7, 8, 8, 9, 9, 9, 11])
+        query = np.array([1, 1, 2, 2, 2, 3, 3, 5, 7, 7, 8, 8, 9, 9, 9, 11])
         answer = {'area': 64,
                   'base_eps': 5,
                   'base_locus': (1, 11),
@@ -148,14 +146,13 @@ class TestHUDC:
                                 'children': None,
                                 'selected': False}],
                   'selected': False}
-        assert query._grow_tree(query.points, query.min_pts, query.max_eps, query.min_eps) == answer
+        assert HUDC._grow_tree(query, 3, 5, 2) == answer  # Minimum eps of 2
 
     def test_child_area(self):
         """
         Test for hidden method _child_area.
         Method modifies tree/node 'child_area' value in place.
         """
-        hudc = HUDC(3, 5, 2)
         query = {'area': 42,
                  'base_eps': 5,
                  'base_locus': (1, 11),
@@ -212,7 +209,7 @@ class TestHUDC:
                                               'selected': False}],
                                 'selected': False}],
                   'selected': False}
-        hudc._child_area(query)
+        HUDC._child_area(query)
         assert query == answer
 
     def test_select_nodes(self):
@@ -220,7 +217,6 @@ class TestHUDC:
         Test for hidden method _select_nodes.
         Method modifies tree/node 'selected' value in place.
         """
-        hudc = HUDC(3, 10, 2)
         query = {'area': 19,
                  'base_eps': 10,
                  'base_locus': (1, 11),
@@ -277,14 +273,13 @@ class TestHUDC:
                                               'selected': False}],
                                 'selected': True}],
                   'selected': False}
-        hudc._select_nodes(query)
+        HUDC._select_nodes(query)
         assert query == answer
 
     def test_retrieve_selected_loci_nested(self):
         """
         Test for hidden method _retrieve_selected_loci using a nested data set.
         """
-        hudc = HUDC(3, 10, 2)
         query = {'area': 19,
                  'base_eps': 10,
                  'base_locus': (1, 11),
@@ -319,13 +314,12 @@ class TestHUDC:
                                'selected': False}],
                  'selected': False}
         answer = [(1, 3), [(6, 9), (11, 11)]]
-        assert hudc._retrieve_selected_loci(query) == answer
+        assert HUDC._retrieve_selected_loci(query) == answer
 
     def test_retrieve_selected_loci_simple(self):
         """
         Test for hidden method _retrieve_selected_loci using a non-nested data set.
         """
-        hudc = HUDC(3, 10, 2)
         query = {'area': 19,
                  'base_eps': 10,
                  'base_locus': (1, 11),
@@ -333,45 +327,41 @@ class TestHUDC:
                  'children': None,
                  'selected': True}
         answer = (1, 11)
-        assert hudc._retrieve_selected_loci(query) == answer
+        assert HUDC._retrieve_selected_loci(query) == answer
 
-    def test_flatten_nested_list(self):
+    def test_flatten_list_nested(self):
         """
-        Test for hidden method _flatten using a nested data set.
-        Method _flatten returns a generator which should be coerced to a list.
+        Test for hidden method _flatten_list using a nested data set.
+        Method _flatten_list returns a generator which should be coerced to a list.
         """
-        hudc = HUDC(3, 10, 2)
         query = [(1, 3), [(6, 9), (11, 11)]]
         answer = [(1, 3), (6, 9), (11, 11)]
-        assert list(hudc._flatten_list(query)) == answer
+        assert list(HUDC._flatten_list(query)) == answer
 
-    def test_flatten_tuple(self):
+    def test_flatten_list_tuple(self):
         """
-        Test for hidden method _flatten using a tuple.
-        Method _flatten returns a generator which should be coerced to a list.
+        Test for hidden method _flatten_list using a tuple.
+        Method _flatten_list returns a generator which should be coerced to a list.
         """
-        hudc = HUDC(3, 10, 2)
         query = (1, 11)
         answer = [(1, 11)]
-        assert list(hudc._flatten_list(query)) == answer
+        assert list(HUDC._flatten_list(query)) == answer
 
     def test_single_hierarchical_cluster(self):
         """
         Test for hidden method _single_hierarchical_cluster.
         """
-        hudc = HUDC(3, 10, 2)
         query = np.array([1, 1, 2, 2, 2, 2, 2, 3, 3, 4, 4, 4, 4, 13, 21, 21, 21, 22, 22, 22, 23, 31], dtype=int)
         answer = np.array([(1, 4), (13, 23)], dtype=_UnivariateLoci._ulocus)
-        npt.assert_array_equal(hudc._single_hierarchical_cluster(query, 3, 10, 2), answer)
+        npt.assert_array_equal(HUDC._single_hierarchical_cluster(query, 3, 10, 2), answer)
 
     def test_hierarchical_cluster(self):
         """
         Test for hidden method _hierarchical_cluster.
         """
-        hudc = HUDC(3, 10, 2)
         query = np.array([1, 2, 21, 22, 22, 22, 24, 38, 54, 54, 55, 56, 65, 65, 66, 67, 68, 90], dtype=int)
         answer = np.array([(21, 24), (54, 56), (65, 68)], dtype=_UnivariateLoci._ulocus)
-        npt.assert_array_equal(hudc._single_hierarchical_cluster(query, 3, 10, 2), answer)
+        npt.assert_array_equal(HUDC._single_hierarchical_cluster(query, 3, 10, 2), answer)
 
     def test_fit(self):
         """
