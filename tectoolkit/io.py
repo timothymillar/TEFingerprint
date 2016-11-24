35fbe337ebb5ea8272d5b10ecdc989f878217bf2
Tim Millar <tim.millar@plantandfood.co.nz>
2016-11-25 11:33:14 +1300

    Docstrings for io.py

--- tectoolkit/io.py
+++ tectoolkit/io.py
@@ -11,12 +11,19 @@ SAM_FLAGS = {'+': ('-F', '20'),
 
 def read_bam_strings(input_bam, reference='', family='', strand='.'):
     """
+    Read a subset of strings from an indexed Bam file.
 
-    :param input_bam:
-    :param reference:
-    :param family:
-    :param strand:
-    :return:
+    :param input_bam: The path to an indexed sorted Bam file
+    :type input_bam: str
+    :param reference: Select reads from a single reference or slice of reference
+    :type reference: str
+    :param family: Select reads that have names beginning with 'family'
+    :type family: str
+    :param strand: Select reads that are found on a specific strand, '+' or '-'
+    :type strand: str
+
+    :return: A list of Sam formatted strings
+    :rtype: list[str]
     """
     flag = SAM_FLAGS[strand]
     sam_strings = np.array(pysam.view(*flag, input_bam, reference).splitlines())
@@ -26,9 +33,13 @@ def read_bam_strings(input_bam, reference='', family='', strand='.'):
 
 def read_bam_references(input_bam):
     """
+    Read all reference names from a Bam file.
 
-    :param input_bam:
-    :return:
+    :param input_bam: The path to an indexed sorted Bam file
+    :type input_bam: str
+
+    :return: A list of reference names
+    :rtype: list[str]
     """
     with pysam.AlignmentFile(input_bam, 'rb') as bam:
         references = bam.references
@@ -37,9 +48,13 @@ def read_bam_references(input_bam):
 
 def read_bam_reference_lengths(input_bam):
     """
+    Read lengths of references from a Bam file
 
-    :param input_bam:
-    :return:
+    :param input_bam: The path to an indexed sorted Bam file
+    :type input_bam: str
+
+    :return: A dictionary of the form {<reference>: <length>}
+    :rtype: dict[str, int]
     """
     with pysam.AlignmentFile(input_bam, 'rb') as bam:
         references = bam.header['SQ']
