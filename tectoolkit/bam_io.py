a9dac2a02df822a0c332102aebfae526af371276
Tim Millar <tim.millar@plantandfood.co.nz>
2017-01-13 13:59:16 +1300

    removed hard coded lowercase conversion for family matching

--- tectoolkit/bam_io.py
+++ tectoolkit/bam_io.py
@@ -20,12 +20,11 @@ def read_bam_strings(input_bam, reference='', family='', strand='.'):
     :return: A list of Sam formatted strings
     :rtype: list[str]
     """
-    family = family.lower()
     SAM_FLAGS = {'+': ('-F', '20'),
                  '-': ('-f', '16', '-F', '4')}
     flag = SAM_FLAGS[strand]
     strings = np.array(pysam.view(*flag, input_bam, reference).splitlines())
-    in_family = np.fromiter((s.split('\tME:Z:')[1].split('\t')[0].lower().startswith(family) for s in strings),
+    in_family = np.fromiter((s.split('\tME:Z:')[1].split('\t')[0].startswith(family) for s in strings),
                             dtype=bool)
     return strings[in_family]
 
