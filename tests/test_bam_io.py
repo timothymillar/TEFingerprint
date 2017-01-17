ccca5e362190728917ae4a667a6b412e660debe3
Tim Millar <tim.millar@plantandfood.co.nz>
2017-01-18 10:10:53 +1300

    migrated tests

--- tests/test_bam_io.py
+++ tests/test_bam_io.py
@@ -113,3 +113,24 @@ def test_flag_orientation(flag, orientation):
 def test_flag_attributes(flag, attributes):
     """Test factory for function flag_attributes"""
     assert bam_io.flag_attributes(flag) == attributes
+
+
+@pytest.mark.parametrize("string,answer",
+                         [('103S111M1D36M', 148)])
+def test_cigar_mapped_length(string, answer):
+    assert bam_io._cigar_mapped_length(string) == answer
+
+
+@pytest.mark.parametrize("strings,answer",
+                         [([
+                               "readA\t0\tchr1\t524\t60\t103S111M1D36M\t*\t0\t0\tGAAATACGT\t1/F11<12G\tNM:i:5\tMD:Z:6T24C35A43^T5T30\tAS:i:120\tXS:i:0\tME:Z:Copia-7_VV",
+                               "readB\t16\tchr1\t600\t0\t103S111M1D36M\t*\t0\t0\tGAAATACGT\t1/F11<12G\tNM:i:5\tMD:Z:6T24C35A43^T5T30\tAS:i:120\tXS:i:0\tME:Z:Copia-7_VV",
+                               "readC\t0\tchr1\t5\t0\t3M3I2M5S\t*\t0\t0\tGAAATACGT\t1/F11<12G\tNM:i:5\tMD:Z:6T24C35A43^T5T30\tAS:i:120\tXS:i:0\tME:Z:Copia-7_VV"],
+                           [(671, 524, '+', 'readA'), (600, 747, '-', 'readB'), (9, 5, '+', 'readC')])])
+def test_parse_sam_strings(strings, answer):
+    """
+    Test factory for hidden method _parse_sam_strings.
+    Tests for parsing sets of forward, reverse and mixed reads.
+    """
+    query = list(bam_io._parse_sam_strings(strings))
+    assert query == answer
