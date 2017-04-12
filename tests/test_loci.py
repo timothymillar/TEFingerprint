915bdab001d4e74eb02b505255b4f5e3b1c7263a
Tim Millar <tim.millar@plantandfood.co.nz>
2017-04-13 11:43:20 +1200

    test for _Loci

--- tests/test_loci.py
+++ tests/test_loci.py
@@ -34,3 +34,90 @@ def test_loci_melter(query, answer):
     answer = np.array(answer, dtype=loci._Loci._DTYPE_LOCI)
     npt.assert_array_equal(query, answer)
 
+
+class TestLoci:
+    """Tests for class _Loci"""
+    def test_split(self):
+        """Split into separate _Loci objects based on keys"""
+        dictionary = {('chr1:0-100', '+', 'Copia'): np.array([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr1:0-100', '-', 'Copia'): np.array([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr1:0-100', '+', 'Gypsy'): np.array([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr1:0-100', '-', 'Gypsy'): np.array([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr2:0-100', '+', 'Copia'): np.array([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr2:0-100', '-', 'Copia'): np.array([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr2:0-100', '+', 'Gypsy'): np.array([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr2:0-100', '-', 'Gypsy'): np.array([(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)],
+                                                             dtype=loci._Loci._DTYPE_LOCI)}
+        query = loci._Loci.from_dict(dictionary)
+        assert len(list(query.split())) == 8
+
+    def test_as_array(self):
+        """"""
+        dictionary = {('chr1:0-100', '+', 'Copia'): np.array([(1, 2), (2, 3)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr1:0-100', '-', 'Copia'): np.array([(1, 2), (2, 3)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr1:0-100', '+', 'Gypsy'): np.array([(1, 2), (2, 3)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr1:0-100', '-', 'Gypsy'): np.array([(1, 2), (2, 3)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr2:0-100', '+', 'Copia'): np.array([(1, 2), (2, 3)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr2:0-100', '-', 'Copia'): np.array([(1, 2), (2, 3)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr2:0-100', '+', 'Gypsy'): np.array([(1, 2), (2, 3)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr2:0-100', '-', 'Gypsy'): np.array([(1, 2), (2, 3)],
+                                                             dtype=loci._Loci._DTYPE_LOCI)}
+        query = loci._Loci.from_dict(dictionary)
+
+        answer = np.array([('chr1:0-100', '+', 'Copia', 1, 2),
+                           ('chr1:0-100', '+', 'Copia', 2, 3),
+                           ('chr1:0-100', '-', 'Copia', 1, 2),
+                           ('chr1:0-100', '-', 'Copia', 2, 3),
+                           ('chr1:0-100', '+', 'Gypsy', 1, 2),
+                           ('chr1:0-100', '+', 'Gypsy', 2, 3),
+                           ('chr1:0-100', '-', 'Gypsy', 1, 2),
+                           ('chr1:0-100', '-', 'Gypsy', 2, 3),
+                           ('chr2:0-100', '+', 'Copia', 1, 2),
+                           ('chr2:0-100', '+', 'Copia', 2, 3),
+                           ('chr2:0-100', '-', 'Copia', 1, 2),
+                           ('chr2:0-100', '-', 'Copia', 2, 3),
+                           ('chr2:0-100', '+', 'Gypsy', 1, 2),
+                           ('chr2:0-100', '+', 'Gypsy', 2, 3),
+                           ('chr2:0-100', '-', 'Gypsy', 1, 2),
+                           ('chr2:0-100', '-', 'Gypsy', 2, 3)], dtype=loci._Loci._DTYPE_ARRAY)
+        npt.assert_array_equal(np.sort(query.as_array()), np.sort(answer))
+
+    def test_as_gff(self):
+        """"""
+        dictionary = {('chr1:0-100', '+', 'Copia'): np.array([(1, 2), (2, 3)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr1:0-100', '-', 'Copia'): np.array([(1, 2), (2, 3)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr1:0-100', '+', 'Gypsy'): np.array([(1, 2), (2, 3)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr1:0-100', '-', 'Gypsy'): np.array([(1, 2), (2, 3)],
+                                                             dtype=loci._Loci._DTYPE_LOCI),
+                      ('chr2:0-100', '+', 'Copia'): np.array([(1, 2), (2, 3)],
+                                                             dtype=loci._Loci._DTYPE_LOCI)}
+        query = loci._Loci.from_dict(dictionary)
+
+        answer = '\n'.join(['chr1\t.\t.\t1\t2\t.\t+\t.\tID=chr1_+_Copia_1;category=Copia',
+                            'chr1\t.\t.\t1\t2\t.\t+\t.\tID=chr1_+_Gypsy_1;category=Gypsy',
+                            'chr1\t.\t.\t1\t2\t.\t-\t.\tID=chr1_-_Copia_1;category=Copia',
+                            'chr1\t.\t.\t1\t2\t.\t-\t.\tID=chr1_-_Gypsy_1;category=Gypsy',
+                            'chr1\t.\t.\t2\t3\t.\t+\t.\tID=chr1_+_Copia_2;category=Copia',
+                            'chr1\t.\t.\t2\t3\t.\t+\t.\tID=chr1_+_Gypsy_2;category=Gypsy',
+                            'chr1\t.\t.\t2\t3\t.\t-\t.\tID=chr1_-_Copia_2;category=Copia',
+                            'chr1\t.\t.\t2\t3\t.\t-\t.\tID=chr1_-_Gypsy_2;category=Gypsy',
+                            'chr2\t.\t.\t1\t2\t.\t+\t.\tID=chr2_+_Copia_1;category=Copia',
+                            'chr2\t.\t.\t2\t3\t.\t+\t.\tID=chr2_+_Copia_2;category=Copia'])
+        assert query.as_gff() == answer
