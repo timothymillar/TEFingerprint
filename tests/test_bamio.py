c25a7170d4f692586b50eef0c339be8aab9b6222
Tim Millar <tim.millar@plantandfood.co.nz>
2017-05-05 12:48:57 +1200

    removed tests for old submodules

---
+++ tests/test_bamio.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+from tectoolkit import bamio
+import pytest
+
+
+@pytest.mark.parametrize("string,answer",
+                         [('103S111M1D36M', 148)])
+def test_cigar_mapped_length(string, answer):
+    assert bamio._cigar_mapped_length(string) == answer
+
+
+@pytest.mark.parametrize("strings,answer",
+                         [([
+                               "readA\t0\tchr1\t524\t60\t103S111M1D36M\t*\t0\t0\tGAAATACGT\t1/F11<12G\tNM:i:5\tMD:Z:6T24C35A43^T5T30\tAS:i:120\tXS:i:0\tME:Z:Copia-7_VV",
+                               "readB\t0\tchr1\t600\t0\t103S111M1D36M\t*\t0\t0\tGAAATACGT\t1/F11<12G\tNM:i:5\tMD:Z:6T24C35A43^T5T30\tAS:i:120\tXS:i:0\tME:Z:Copia-7_VV",
+                               "readC\t0\tchr1\t5\t0\t3M3I2M5S\t*\t0\t0\tGAAATACGT\t1/F11<12G\tNM:i:5\tMD:Z:6T24C35A43^T5T30\tAS:i:120\tXS:i:0\tME:Z:Copia-7_VV"],
+                           [(524, 671, 'readA'), (600, 747, 'readB'), (5, 9, 'readC')])])
+def test_parse_read_loci(strings, answer):
+    """
+    Test factory for hidden method _parse_sam_strings.
+    Tests for parsing sets of forward, reverse and mixed reads.
+    """
+    query = list(bamio._parse_read_loci(strings))
+    assert query == answer
