86833af72bfa4e45a86a7c5312764b90defdff75
Tim Millar <tim.millar@plantandfood.co.nz>
2017-07-19 15:33:02 +1200

    split preprocess into job subclasses

--- tefingerprint/preprocess.py
+++ tefingerprint/preprocess.py
@@ -8,14 +8,11 @@ import shutil
 import sys
 import re
 from tempfile import mkdtemp
-from functools import reduce
-from itertools import chain, tee
-
 
 
 class PreProcessProgram(object):
     """"""
-    def __init__(self, dangler_bam,
+    def __init__(self, input_bam,
                  reference_fasta,
                  output_bam,
                  include_tails=True,
@@ -23,13 +20,13 @@ class PreProcessProgram(object):
                  mate_element_tag='ME',
                  temp_dir=False,
                  threads=1):
-        self.dangler_bam = dangler_bam
+        self.input_bam = input_bam
         self.reference_fasta = reference_fasta
         self.output_bam = output_bam
         self.include_tails = include_tails
         self.tail_minimum_length = tail_minimum_length
         self.mate_element_tag = mate_element_tag
-        self.temp_dir = temp_dir
+        self.TEMP_DIR = temp_dir
         self.threads = str(threads)
 
     @staticmethod
@@ -99,55 +96,25 @@ class PreProcessProgram(object):
                                  temp_dir=arguments.tempdir[0],
                                  threads=arguments.threads[0])
 
-    def _run_pipeline(self, scratch_dir):
+    def _run_pipeline(self, SCRATCH_DIR):
         """"""
 
-        temp_fastq = os.path.join(scratch_dir, '1_danglerReads.fastq')
-        print('>>> Writing dangler reads to: {0}'.format(temp_fastq))
-
-        # extract danglers from bam
-        read_sources = []  # a list to accumulate generators
-        print('>>> Extracting dangler reads from: {0}'.format(self.dangler_bam))
-        read_sources.append(extract_forward_danglers(self.dangler_bam))
-        read_sources.append(extract_reverse_danglers(self.dangler_bam))
-
         if self.include_tails:
-            # extract soft clipped tails from bam
-            print('>>> Extracting soft clipped tails from: {0}'.format(self.dangler_bam))
-            read_sources.append(extract_forward_soft_clips(self.dangler_bam, min_length=self.tail_minimum_length))
-            read_sources.append(extract_reverse_soft_clips(self.dangler_bam, min_length=self.tail_minimum_length))
+            jobs = ProcessForwardDanglers, ProcessReverseDanglers, ProcessForwardTailClips, ProcessReverseTailClips
+        else:
+            jobs = ProcessForwardDanglers, ProcessReverseDanglers
 
-        # chain read sources into single generator
-        reads = reduce(chain, read_sources)
+        bams = []  # used to accumulate names of bam from each job
 
-        # split read generator into two new generators
-        reads_for_fastq, reads_for_tags = tee(reads)
+        for job in jobs:
+            # run each job and append resulting bam file to the list
+            bams.append(job(self.input_bam,
+                            self.reference_fasta,
+                            SCRATCH_DIR).run())
 
-        # store read read mate elements as dict
-        mate_element_dict = get_mate_elements(reads_for_tags)
+        print('>>> Writing: {0}'.format(self.output_bam))
+        merge_bams(self.output_bam, *bams)
 
-        # write temp fastq
-        print('>>> Writing dangler reads to: {0}'.format(temp_fastq))
-        with open(temp_fastq, 'a') as f:
-            f.writelines(reads_as_fastq(reads_for_fastq))
-
-        # map danglers to reference
-        temp_bam = os.path.join(scratch_dir, '2_danglerReadsMappedToReference.bam')
-        print('>>> Mapping dangler reads to reference at: {0}'.format(temp_bam))
-        map_danglers_to_reference(temp_fastq,
-                                  self.reference_fasta,
-                                  temp_bam,
-                                  self.threads)
-
-        # index bam
-        print('>>> Indexing: {0}'.format(temp_bam))
-        index_bam(temp_bam)
-
-        # tag danglers and write output file
-        print('>>> Adding tags to mapped danglers at: {0}'.format(self.output_bam))
-        tag_danglers(temp_bam, mate_element_dict, self.output_bam, self.mate_element_tag)
-
-        # index bam
         print('>>> Indexing: {0}'.format(self.output_bam))
         index_bam(self.output_bam)
 
@@ -157,21 +124,22 @@ class PreProcessProgram(object):
         _check_programs_installed('bwa', 'samtools')
 
         # create temp dir for intermediate files unless one is supplied by user
-        if self.temp_dir:
-            if not os.path.exists(self.temp_dir):
-                os.makedirs(self.temp_dir)
-            temp_dir = self.temp_dir
+        if self.TEMP_DIR:
+            if not os.path.exists(self.TEMP_DIR):
+                os.makedirs(self.TEMP_DIR)
+            temp_dir = self.TEMP_DIR
             print('>>> Using directory at: {0}'.format(temp_dir))
         else:
             temp_dir = mkdtemp()
             print('>>> Creating temporary directory at: {0}'.format(temp_dir))
 
         # attempt running pipeline
+        print('>>> Running Pipeline')
         try:
             self._run_pipeline(temp_dir)
         except:
             # remove temp dir unless it was supplied by user
-            if self.temp_dir:
+            if self.TEMP_DIR:
                 print('>>> Error Encountered... temporary files kept at: {0}'.format(temp_dir))
             else:
                 print('>>> Error Encountered... Removing temporary directory at: {0}'.format(temp_dir))
@@ -180,7 +148,7 @@ class PreProcessProgram(object):
             raise
 
         # remove temp dir unless it was supplied by user
-        if self.temp_dir:
+        if self.TEMP_DIR:
             pass
         else:
             print('>>> Removing temporary directory at: {0}'.format(temp_dir))
@@ -215,6 +183,19 @@ def index_bam(bam):
     """
     _check_programs_installed('samtools')
     return subprocess.call(['samtools index ' + bam], shell=True)
+
+
+def merge_bams(output_bam, *args):
+    input_bams = ' '.join(args)
+    return subprocess.call(['samtools merge -f {0} {1}'.format(output_bam, input_bams)], shell=True)
+
+
+def slice_bam(input_bam, flags, output_bam):
+    _check_programs_installed('samtools')
+    if isinstance(flags, list):
+        flags = ' '.join(flags)
+    command = 'samtools view -Su {0} {1} | samtools sort - -o {2}'.format(flags, input_bam, output_bam)
+    return subprocess.call([command], shell=True)
 
 
 def map_pairs_to_repeat_elements(fastq_1, fastq_2, repeats_fasta, output_bam, threads=1):
@@ -288,6 +269,217 @@ def reads_as_fastq(read_dicts):
 
 def get_mate_elements(read_dicts):
     return {read['QNAME']: read['RNAME'] for read in read_dicts}
+
+
+class _ProcessDanglers(object):
+    EXTRACT_DANGLER_FLAGS = ''
+    TEMP_FILE_PREFIX = '_'
+
+    def __init__(self,
+                 input_bam,
+                 reference,
+                 directory,
+                 remove_files=True,
+                 bwa_threads=1,
+                 element_tag='ME',
+                 soft_clip_min_length=38):
+        self.input_bam = input_bam
+        self.reference = reference
+        self.directory = directory
+        self.remove_files = remove_files
+        self.temp_dangler_bam = os.path.join(self.directory,
+                                             self.TEMP_FILE_PREFIX +
+                                             '0_danglers.bam')
+        self.temp_fastq = os.path.join(self.directory,
+                                       self.TEMP_FILE_PREFIX +
+                                       '1_danglers.fastq')
+        self.temp_mapped_danglers_bam = os.path.join(self.directory,
+                                                     self.TEMP_FILE_PREFIX +
+                                                     '2_mapped_danglers.bam')
+        self.temp_tagged_mapped_danglers_bam = os.path.join(self.directory,
+                                                            self.TEMP_FILE_PREFIX +
+                                                            '3_tagged_mapped_danglers.bam')
+        self.bwa_threads = bwa_threads
+        self.element_tag = element_tag
+        self.soft_clip_min_length = soft_clip_min_length
+        self.element_tags = {}
+
+    @staticmethod
+    def _index_bam(bam):
+        return subprocess.check_call(['samtools index ' + bam], shell=True)
+
+    def _subset_bam(self):
+        print('subsetting {0} to {1}'.format(self.input_bam, self.temp_dangler_bam))
+        command = 'samtools view -Su {0} {1} | samtools sort - -o {2}'.format(self.EXTRACT_DANGLER_FLAGS,
+                                                                              self.input_bam,
+                                                                              self.temp_dangler_bam)
+        code = subprocess.check_call([command], shell=True)
+        self._index_bam(self.temp_dangler_bam)
+        print('indexed {0}'.format(self.temp_dangler_bam))
+        return code
+
+    def _parse_reads(self, reads):
+        """
+        Converts a iterator pysam reads to a a generator of dicts.
+
+        Any additional processing or filtering should be done here.
+
+        :param reads: iterator of pysam reads
+        :return: generator of dicts
+        :rtype: generator[str, str]
+        """
+        for read in reads:
+            yield {'name': read.qname,
+                   'element': read.reference_name,
+                   'sequence': read.seq,
+                   'quality': read.qual}
+
+    def _extract_danglers(self):
+        with open(self.temp_fastq, 'a') as fq:
+            reads = pysam.Samfile(self.temp_dangler_bam, "rb")
+            reads = self._parse_reads(reads)
+            for read in reads:
+
+                # add read name and element name to dict
+                self.element_tags[read['name']] = read['element']
+
+                # write read to fastq
+                fq.write("@{0}\n{1}\n+\n{2}\n".format(read['name'],
+                                                      read['sequence'],
+                                                      read['quality']))
+
+    def _map_danglers_to_reference(self):
+        command = 'bwa mem -t {0} {1} {2} | samtools view -Su - | samtools sort - -o {3}'
+        command = command.format(self.bwa_threads,
+                                 self.reference,
+                                 self.temp_fastq,
+                                 self.temp_mapped_danglers_bam)
+        code = subprocess.call([command], shell=True)
+        self._index_bam(self.temp_mapped_danglers_bam)
+        return code
+
+    def _tag_danglers(self):
+        danglers_untagged = pysam.Samfile(self.temp_mapped_danglers_bam, "rb")
+        danglers_tagged = pysam.Samfile(self.temp_tagged_mapped_danglers_bam, "wb", template=danglers_untagged)
+        for read in danglers_untagged.fetch():
+            read.tags += [(self.element_tag, self.element_tags[read.qname])]
+            danglers_tagged.write(read)
+        danglers_untagged.close()
+        danglers_tagged.close()
+        self._index_bam(self.temp_tagged_mapped_danglers_bam)
+
+    def run(self):
+        self._subset_bam()
+        self._extract_danglers()
+        self._map_danglers_to_reference()
+        self._tag_danglers()
+        return self.temp_tagged_mapped_danglers_bam
+
+
+class ProcessForwardDanglers(_ProcessDanglers):
+    EXTRACT_DANGLER_FLAGS = '-F 2304 -F 16 -F 8 -f 4'
+    TEMP_FILE_PREFIX = 'forward_'
+
+    def _parse_reads(self, reads):
+        """
+        Converts a iterator pysam reads to a a generator of dicts.
+
+        Any additional processing or filtering should be done here.
+
+        :param reads: iterator of pysam reads
+        :return: generator of dicts
+        :rtype: generator[str, str]
+        """
+        for read in reads:
+            yield {'name': read.qname,
+                   'element': read.reference_name,
+                   'sequence': read.seq,
+                   'quality': read.qual}
+
+
+class ProcessReverseDanglers(_ProcessDanglers):
+    EXTRACT_DANGLER_FLAGS = '-F 2304 -f 16 -F 8 -f 4'
+    TEMP_FILE_PREFIX = 'reverse_'
+
+    def _parse_reads(self, reads):
+        """
+        Converts a iterator pysam reads to a a generator of dicts.
+
+        Any additional processing or filtering should be done here.
+
+        :param reads: iterator of pysam reads
+        :return: generator of dicts
+        :rtype: generator[str, str]
+        """
+        for read in reads:
+            yield {'name': read.qname,
+                   'element': read.reference_name,
+                   'sequence': reverse_complement(read.seq),
+                   'quality': read.qual[::-1]}
+
+
+class ProcessForwardTailClips(_ProcessDanglers):
+    EXTRACT_DANGLER_FLAGS = '-F 2304 -f 2 -F 16 -F 4 -F 8'
+    TEMP_FILE_PREFIX = 'forward_tail_clips_'
+
+    def _parse_reads(self, reads):
+        """
+        Converts a iterator pysam reads to a a generator of dicts.
+
+        Any additional processing or filtering should be done here.
+
+        :param reads: iterator of pysam reads
+        :return: generator of dicts
+        :rtype: generator[str, str]
+        """
+        for read in reads:
+            clip = re.findall(r"^[0-9]+[S]", read.cigarstring)
+            # if there is a soft clipped section
+            if clip:
+                # get clip length
+                clip = int(clip[0].strip('S'))
+                if clip >= self.soft_clip_min_length:
+                    # return the clipped section as a read
+                    yield {'name': read.qname,
+                           'element': read.reference_name,
+                           'sequence': read.seq[0: clip + 1],
+                           'quality': read.qual[0: clip + 1]}
+                else:
+                    pass
+            else:
+                pass
+
+
+class ProcessReverseTailClips(_ProcessDanglers):
+    EXTRACT_DANGLER_FLAGS = '-F 2304 -f 2 -f 16 -F 4 -F 8'
+    TEMP_FILE_PREFIX = 'reverse_tail_clips_'
+
+    def _parse_reads(self, reads):
+        """
+        Converts a iterator pysam reads to a a generator of dicts.
+
+        Any additional processing or filtering should be done here.
+
+        :param reads: iterator of pysam reads
+        :return: generator of dicts
+        :rtype: generator[str, str]
+        """
+        for read in reads:
+            clip = re.findall(r"[0-9]+[S]$", read.cigarstring)
+            # if there is a soft clipped section
+            if clip:
+                # get clip length
+                clip = int(clip[0].strip('S'))
+                if clip >= self.soft_clip_min_length:
+                    # return the clipped section as a read
+                    yield {'name': read.qname,
+                           'element': read.reference_name,
+                           'sequence': reverse_complement(read.seq[0: clip + 1]),
+                           'quality': read.qual[0: clip + 1][::-1]}
+                else:
+                    pass
+            else:
+                pass
 
 
 def extract_forward_danglers(bam):
