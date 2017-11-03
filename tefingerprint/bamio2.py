4b3656d03b36283ec35e87648e9a2984878f4ea2
Tim Millar <tim.millar@plantandfood.co.nz>
2017-11-03 15:00:32 +1300

    use known tes when extracting anchors

--- tefingerprint/bamio2.py
+++ tefingerprint/bamio2.py
@@ -6,6 +6,7 @@ import numpy as np
 from itertools import product
 from collections import deque
 from tefingerprint import loci2
+from tefingerprint import interval
 
 
 def extract_bam_references(bam):
@@ -75,7 +76,7 @@ def extract_informative_read_tips(bams, references, categories, quality=0, tag='
                              for header, data in dictionary.items()))
 
 
-def _parse_bam_anchor_insert_data(bam, reference, max_size, quality=0):
+def _parse_bam_anchor_insert_data(bam, reference, quality=0):
     with pysam.AlignmentFile(bam, 'rb') as bam:
         for r in bam.fetch(region=reference):
             if not r.is_reverse \
@@ -86,26 +87,41 @@ def _parse_bam_anchor_insert_data(bam, reference, max_size, quality=0):
                     and not r.mate_is_unmapped \
                     and r.reference_id == r.next_reference_id \
                     and r.mapping_quality >= quality \
-                    and r.mpos > r.blocks[-1][-1] \
-                    and r.mpos - r.blocks[-1][-1] < max_size:
+                    and r.mpos > r.blocks[-1][-1]:
                 yield r.blocks[-1][-1], r.mpos + 1
 
 
-def extract_anchor_intervals(bams, references, max_size, quality=0):
+def extract_anchor_intervals(bams, references, known_transposons, insert_size, quality=0):
     if isinstance(bams, str):
         bams = [bams]
 
     if isinstance(references, str):
         references = [references]
 
+    # simplify known transposon headers for comparison
+    known_transposons = known_transposons.map(lambda x: loci2.mutate_header(x, strand='.', category=None, source=None),
+                                              append_duplicate_headers=True)
+
     jobs = product(bams, references)
     dtype = np.dtype([('start', np.int64), ('stop', np.int64)])
     intervals = loci2.ContigSet()
 
     for bam, reference in jobs:
-        header = loci2.Header(reference=reference.split(':')[0], source=os.path.basename(bam))
-        data = _parse_bam_anchor_insert_data(bam, reference, max_size, quality=quality)
-        intervals.add(loci2.unions(loci2.Contig(header=header, array=np.fromiter(data, dtype=dtype))))
+        header = loci2.Header(reference=reference.split(':')[0], source=os.path.basename(bam), strand='.')
+        anchors = np.fromiter(_parse_bam_anchor_insert_data(bam, reference, quality=quality), dtype=dtype)
+        anchor_lengths = interval.lengths(anchors)
+
+        # calculate lengths on known tranposons within each anchor interval
+        local_transposons_header = loci2.Header(reference=reference.split(':')[0], strand='.')
+        local_known_transposons = known_transposons[local_transposons_header]
+        contained_known_te_lengths = interval.length_of_contains(anchors, local_known_transposons.loci)
+
+        # filter anchors based on insert size
+        adjusted_anchor_lengths = anchor_lengths - contained_known_te_lengths
+        anchors = anchors[adjusted_anchor_lengths <= insert_size]
+
+        # use unions of filtered anchors as loci
+        intervals.add(loci2.unions(loci2.Contig(header=header, array=anchors)))
 
     return intervals
 
