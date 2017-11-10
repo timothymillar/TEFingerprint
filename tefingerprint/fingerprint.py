b6f8b74932e93c3c97e894894902f7467121990c
Tim Millar <tim.millar@plantandfood.co.nz>
2017-11-10 15:04:01 +1300

    docstrings

--- tefingerprint/fingerprint.py
+++ tefingerprint/fingerprint.py
@@ -10,6 +10,35 @@ from tefingerprint import loci2
 
 
 def count_reads(clusters, reads, trim=True, n_common_elements=0):
+    """
+    Counts the read tips contained in each cluster (locus).
+
+    A total count of reads across all source (bam) files is calculated.
+    The median read position amoung all reads is calculated.
+    The n_common_elements parameter may be used to include counts of the 'n' most common elements found in each sample.
+    By default, clusters are trimmed to the most distant reads they contain.
+
+    Fields required in 'clusters':
+        'start': int, 'stop': int, 'median': int
+
+    Fields required in 'reads':
+        'tip': int, 'element': str, 'source': str
+
+    Fields appended to return value:
+        'median': int, 'sample': ('name': str, 'count': int, 'element': ( 'name': str, 'count': int))
+
+    :param clusters: a collection of cluster loci (intervals)
+    :type clusters: :class:`loci2.ContigSet`
+    :param reads: a collection of reads (tip positions) from one or more bam files
+    :type reads: :class:`loci2.ContigSet`
+    :param trim: if true the boundaries of each cluster are trimed to the range of the reads they contain
+    :type trim: bool
+    :param n_common_elements: specifies how many of the most common elements to count for each sample
+    :type n_common_elements: int
+
+    :return: a collection of cluster loci (intervals) with read counts
+    :rtype: :class:`loci2.ContigSet`
+    """
     sources = np.array(list({ctg.header.source for ctg in list(reads.contigs())}))
     sources.sort()
 
@@ -72,11 +101,12 @@ def count_reads(clusters, reads, trim=True, n_common_elements=0):
     return clusters
 
 
-def _known_insertion_matcher(contig, known_insertions, buffer=0):
+def _known_insertion_matcher(contig, known_insertions, distance=0):
+    """matches clusters on a contig to known elements"""
     if contig.header.strand == '+':
         for cluster in contig.loci:
             mask = interval.singular_intersects_lower_bound(cluster['median'],
-                                                            cluster['stop'] + buffer,
+                                                            cluster['stop'] + distance,
                                                             known_insertions.loci)
             if np.any(mask):
                 yield known_insertions.loci['element'][mask][0]
@@ -84,7 +114,7 @@ def _known_insertion_matcher(contig, known_insertions, buffer=0):
                 yield ''
     elif contig.header.strand == '-':
         for cluster in contig.loci:
-            mask = interval.singular_intersects_upper_bound(cluster['start'] - buffer,
+            mask = interval.singular_intersects_upper_bound(cluster['start'] - distance,
                                                             cluster['median'],
                                                             known_insertions.loci)
             if np.any(mask):
@@ -93,8 +123,33 @@ def _known_insertion_matcher(contig, known_insertions, buffer=0):
                 yield ''
 
 
-def match_known_insertions(clusters, known_insertions, buffer=0):
+def match_known_insertions(clusters, known_insertions, distance=0):
+    """
+    Match clusters indicating insertions to known insertions annotated in the genome.
 
+    Known insertions are represented as an object of :class:`loci2.ContigSet` created from a gff file.
+    Clusters are matched to a known insertion if they are for the same category and are within the specified distance
+    of the insertions end.
+
+    Fields required in 'clusters':
+        'start': int, 'stop': int, 'median': int
+
+    Fields required in 'known_insertions':
+        'start': int, 'stop': int, 'element': str
+
+    Fields appended to return value:
+        'known_element': str
+
+    :param clusters: a collection of cluster loci (intervals)
+    :type clusters: :class:`loci2.ContigSet`
+    :param known_insertions: a collection of cluster loci (intervals)
+    :type known_insertions: :class:`loci2.ContigSet`
+    :param distance: maximum distance for connecting a cluster to a known insertion
+    :type distance: int
+
+    :return: a collection of cluster loci (intervals) tagged with known insertions
+    :rtype: :class:`loci2.ContigSet`
+    """
     matched = loci2.ContigSet()
 
     # make known insertion headers un-stranded and drop origin file
@@ -106,7 +161,7 @@ def match_known_insertions(clusters, known_insertions, buffer=0):
         # get relevant known insertions
         known = known_insertions[contig.header.mutate(strand='.')]
 
-        matches = np.array(list(_known_insertion_matcher(contig, known, buffer=buffer)))
+        matches = np.array(list(_known_insertion_matcher(contig, known, distance=distance)))
         matches = np.array(matches,
                            dtype=np.dtype([('known_element', '<O')]))
 
@@ -115,7 +170,7 @@ def match_known_insertions(clusters, known_insertions, buffer=0):
     return matched
 
 
-def _cluster_pairer(forward, reverse, buffer=0, use_known_elements=True):
+def _cluster_pairer(forward, reverse, distance=0, use_known_elements=True):
     """sorts clusters into pairs with knowledge of known insertions or distances"""
     dtype_sort = np.dtype([('start', np.int64),
                            ('stop', np.int64),
@@ -166,7 +221,7 @@ def _cluster_pairer(forward, reverse, buffer=0, use_known_elements=True):
                 if prev['known_element'] != '' and prev['known_element'] == clust['known_element']:
                     # clusters match same known element
                     yield (prev['index'], clust['index'])
-                elif prev['stop'] + buffer >= clust['start'] - buffer:
+                elif prev['stop'] + distance >= clust['start'] - distance:
                     # they are within reasonable range of one another (2 * buffer)
                     yield (prev['index'], clust['index'])
                 else:
@@ -179,12 +234,29 @@ def _cluster_pairer(forward, reverse, buffer=0, use_known_elements=True):
         yield (prev['index'], None)
 
 
-def pair_clusters(clusters, buffer=0, use_known_elements=True):
+def pair_clusters(clusters, distance=0, use_known_elements=True):
     """
+    Join matching clusters on opposite strands.
+
+    Clusters of the same calgary are joined if they are within 2 * distance of one another.
+    Clusters may also be joined if they have both been matched to the same known element.
+
+    Fields required in 'clusters':
+        'start': int, 'stop': int, 'median': int, 'known_element': str
+
+    Fields appended to return value:
+        'ID': str, 'paired' int
 
 
-    :param clusters: a set of contigs that are not un-stranded (i.e. are on the + or - strand)
-    :return:
+    :param clusters: a collection of cluster loci (intervals)
+    :type clusters: :class:`loci2.ContigSet`
+    :param distance: the distance to search out from each cluster
+    :type distance: int
+    :param use_known_elements: specify whether to join pairs based on a common known element (default: True)
+    :type use_known_elements: bool
+
+    :return: a collection of cluster loci (intervals) with 'ID' and 'paired' fields
+    :rtype: :class:`loci2.ContigSet`
     """
     joint_clusters = loci2.ContigSet()
 
@@ -202,7 +274,7 @@ def pair_clusters(clusters, buffer=0, use_known_elements=True):
         reverse = clusters[header.mutate(strand='-')]
 
         # sort them into pairs based on median
-        pairs = _cluster_pairer(forward, reverse, buffer=buffer, use_known_elements=use_known_elements)
+        pairs = _cluster_pairer(forward, reverse, distance=distance, use_known_elements=use_known_elements)
 
         # create arrays for the new data
         forward_join_data = np.empty(len(forward), dtype=dtype_join_data)
