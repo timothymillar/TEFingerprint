5b3a74eb3acaa51f3bf9c4de720cd4fd25489776
Tim Millar <tim.millar@plantandfood.co.nz>
2017-12-12 15:52:24 +1300

    docstrings

--- tefingerprint/gff.py
+++ tefingerprint/gff.py
@@ -12,24 +12,60 @@ _ATTRIBUTE_ENCODING = _COLUMN_ENCODING + [(';', '%3B'),
 
 
 def encode_column(string):
+    """
+    Escape special symbols for first 8 gff3 columns
+
+    :param string: value of column
+    :type string: str
+
+    :return: escaped string
+    :rtype str:
+    """
     for symbol, escape in _COLUMN_ENCODING:
         string = string.replace(symbol, escape)
     return string
 
 
 def decode_column(string):
+    """
+    Un-escape special symbols for first 8 gff3 columns
+
+    :param string: value of column
+    :type string: str
+
+    :return: un-escaped string
+    :rtype str:
+    """
     for symbol, escape in _COLUMN_ENCODING:
         string = string.replace(escape, symbol)
     return string
 
 
 def encode_attribute(string):
+    """
+    Escape special symbols for gff3 attribute key or value
+
+    :param string: key or value of attribute
+    :type string: str
+
+    :return: escaped string
+    :rtype str:
+    """
     for symbol, escape in _ATTRIBUTE_ENCODING:
         string = string.replace(symbol, escape)
     return string
 
 
 def decode_attribute(string):
+    """
+    Un-escape special symbols for gff3 attribute key or value
+
+    :param string: key or value of attribute
+    :type string: str
+
+    :return: un-escaped string
+    :rtype str:
+    """
     for symbol, escape in _ATTRIBUTE_ENCODING:
         string = string.replace(escape, symbol)
     return string
