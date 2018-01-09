ef80958b6cf862b1cdd9bafe3d614a27dfa7332f
Tim Millar <tim.millar@plantandfood.co.nz>
2018-01-09 15:26:43 +1300

    tests

--- tests/test_tefingerprint.py
+++ tests/test_tefingerprint.py
@@ -28,19 +28,25 @@ class TestTEFingerprint:
         assert output.decode() == answer
 
     @pytest.mark.parametrize('query,answer',
-                             [(['tefingerprint', DATA_PATH + '/testA-2017-06-08.bam',  DATA_PATH + 'testB-2017-06-08.bam', '-f', 'Gypsy', 'PIF', '-e', '30000', '-m', '5', '-b', '10000', '-n', '0', '--gff', '-'],
+                             [(['tefingerprint', DATA_PATH + '/testA-2017-06-08.bam',  DATA_PATH + 'testB-2017-06-08.bam', '-f', 'Gypsy', 'PIF', '-e', '30000', '-m', '5', '-b', '10000', '-n', '0', '--no-colour', '--gff', '-'],
                                ('chr1\tTEFingerprint\t.\t2452\t24217\t.\t+\t.\tcategory=Gypsy;median=2973;sample_0_name=testA-2017-06-08.bam;sample_0_count=18;sample_1_name=testB-2017-06-08.bam;sample_1_count=20;ID=Gypsy_chr1_+_24217;pair=Gypsy_chr1_-_3217\n'
                                 'chr1\tTEFingerprint\t.\t3217\t3800\t.\t-\t.\tcategory=Gypsy;median=3525;sample_0_name=testA-2017-06-08.bam;sample_0_count=8;sample_1_name=testB-2017-06-08.bam;sample_1_count=0;ID=Gypsy_chr1_-_3217;pair=Gypsy_chr1_+_24217\n'
                                 'chr1\tTEFingerprint\t.\t21834\t21982\t.\t-\t.\tcategory=PIF;median=21956;sample_0_name=testA-2017-06-08.bam;sample_0_count=4;sample_1_name=testB-2017-06-08.bam;sample_1_count=6;ID=PIF_chr1_-_21834;pair=.\n'
                                 'chr1\tTEFingerprint\t.\t24787\t24919\t.\t-\t.\tcategory=Gypsy;median=24872;sample_0_name=testA-2017-06-08.bam;sample_0_count=12;sample_1_name=testB-2017-06-08.bam;sample_1_count=10;ID=Gypsy_chr1_-_24787;pair=.\n')
                                ),
-                              (['tefingerprint', DATA_PATH + '/testA-2017-06-08.bam',  DATA_PATH + 'testB-2017-06-08.bam', '-f', 'Gypsy', 'PIF', '-e', '30000', '-m', '5', '-b', '100000', '-n', '0', '--gff', '-'],
+                              (['tefingerprint', DATA_PATH + '/testA-2017-06-08.bam', DATA_PATH + 'testB-2017-06-08.bam', '-f', 'Gypsy', 'PIF', '-e', '30000', '-m', '5', '-b', '10000', '-n', '0', '--gff', '-'],
+                              ('chr1\tTEFingerprint\t.\t2452\t24217\t.\t+\t.\tcategory=Gypsy;median=2973;sample_0_name=testA-2017-06-08.bam;sample_0_count=18;sample_1_name=testB-2017-06-08.bam;sample_1_count=20;color=#C6DBEF;ID=Gypsy_chr1_+_24217;pair=Gypsy_chr1_-_3217\n'
+                               'chr1\tTEFingerprint\t.\t3217\t3800\t.\t-\t.\tcategory=Gypsy;median=3525;sample_0_name=testA-2017-06-08.bam;sample_0_count=8;sample_1_name=testB-2017-06-08.bam;sample_1_count=0;color=#08306B;ID=Gypsy_chr1_-_3217;pair=Gypsy_chr1_+_24217\n'
+                               'chr1\tTEFingerprint\t.\t21834\t21982\t.\t-\t.\tcategory=PIF;median=21956;sample_0_name=testA-2017-06-08.bam;sample_0_count=4;sample_1_name=testB-2017-06-08.bam;sample_1_count=6;color=#9ECAE1;ID=PIF_chr1_-_21834;pair=.\n'
+                               'chr1\tTEFingerprint\t.\t24787\t24919\t.\t-\t.\tcategory=Gypsy;median=24872;sample_0_name=testA-2017-06-08.bam;sample_0_count=12;sample_1_name=testB-2017-06-08.bam;sample_1_count=10;color=#C6DBEF;ID=Gypsy_chr1_-_24787;pair=.\n')
+                               ),
+                              (['tefingerprint', DATA_PATH + '/testA-2017-06-08.bam',  DATA_PATH + 'testB-2017-06-08.bam', '-f', 'Gypsy', 'PIF', '-e', '30000', '-m', '5', '-b', '100000', '-n', '0', '--no-colour', '--gff', '-'],
                                ('chr1\tTEFingerprint\t.\t2452\t24217\t.\t+\t.\tcategory=Gypsy;median=2973;sample_0_name=testA-2017-06-08.bam;sample_0_count=18;sample_1_name=testB-2017-06-08.bam;sample_1_count=20;ID=Gypsy_chr1_+_24217;pair=Gypsy_chr1_-_3217\n'
                                 'chr1\tTEFingerprint\t.\t3217\t24919\t.\t-\t.\tcategory=Gypsy;median=24857;sample_0_name=testA-2017-06-08.bam;sample_0_count=20;sample_1_name=testB-2017-06-08.bam;sample_1_count=10;ID=Gypsy_chr1_-_3217;pair=Gypsy_chr1_+_24217\n'
                                 'chr1\tTEFingerprint\t.\t21834\t21982\t.\t-\t.\tcategory=PIF;median=21956;sample_0_name=testA-2017-06-08.bam;sample_0_count=4;sample_1_name=testB-2017-06-08.bam;sample_1_count=6;ID=PIF_chr1_-_21834;pair=.\n')
                                )
                               ],
-                             ids=['no-overlap', 'overlap'])
+                             ids=['no-overlap', 'colour', 'overlap'])
     def test_buffer(self, query, answer):
         output, error = subprocess.Popen(query, stdout=subprocess.PIPE,
                                          stderr=subprocess.PIPE).communicate()
